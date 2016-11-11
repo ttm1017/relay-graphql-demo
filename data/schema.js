0b8a378fd2b31fbd36c596400f4a96a52c0beeb3
ttm1017 <463786395@qq.com>
2016-11-11 10:49:19 +0800

    finish charts to show data

--- data/schema.js
+++ data/schema.js
@@ -1,14 +1,3 @@
-/**
- * This file provided by Facebook is for non-commercial testing and evaluation
- * purposes only.  Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
 'use strict';
 
@@ -19,6 +8,7 @@ import {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
+  GraphQLInt
 } from 'graphql';
 
 import {
@@ -33,217 +23,34 @@ import {
 } from 'graphql-relay';
 
 import {
-    getLUKE,
     getCharacter,
-    getAllCharacter
+    getAllCharacter,
+    getPeriodCharactors,
+    getPeriodCharactor,
+    getData
 } from './database';
 
-/**
- * This is a basic end-to-end test, designed to demonstrate the various
- * capabilities of a Relay-compliant GraphQL server.
- *
- * It is recommended that readers of this test be familiar with
- * the end-to-end test in GraphQL.js first, as this test skips
- * over the basics covered there in favor of illustrating the
- * key aspects of the Relay spec that this test is designed to illustrate.
- *
- * We will create a GraphQL schema that describes the major
- * factions and ships in the original Star Wars trilogy.
- *
- * NOTE: This may contain spoilers for the original Star
- * Wars trilogy.
- */
-
-/**
- * Using our shorthand to describe type systems,
- * the type system for our example will be the following:
- *
- * interface Node {
- *   id: ID!
- * }
- *
- * type Faction : Node {
- *   id: ID!
- *   name: String
- *   ships: ShipConnection
- * }
- *
- * type Ship : Node {
- *   id: ID!
- *   name: String
- * }
- *
- * type ShipConnection {
- *   edges: [ShipEdge]
- *   pageInfo: PageInfo!
- * }
- *
- * type ShipEdge {
- *   cursor: String!
- *   node: Ship
- * }
- *
- * type PageInfo {
- *   hasNextPage: Boolean!
- *   hasPreviousPage: Boolean!
- *   startCursor: String
- *   endCursor: String
- * }
- *
- * type Query {
- *   rebels: Faction
- *   empire: Faction
- *   node(id: ID!): Node
- * }
- *
- * input IntroduceShipInput {
- *   clientMutationId: string!
- *   shipName: string!
- *   factionId: ID!
- * }
- *
- * input IntroduceShipPayload {
- *   clientMutationId: string!
- *   ship: Ship
- *   faction: Faction
- * }
- *
- * type Mutation {
- *   introduceShip(input IntroduceShipInput!): IntroduceShipPayload
- * }
- */
-
-/**
- * We get the node interface and field from the Relay library.
- *
- * The first method defines the way we resolve an ID to its object.
- * The second defines the way we resolve a node object to its GraphQL type.
- */
 const {nodeInterface, nodeField} = nodeDefinitions(
   (globalId) => {
     const {type, id} = fromGlobalId(globalId);
-    if (type === 'Faction') {
-      return getFaction(id);
-    } else if (type === 'Ship ') {
-      return getShip(id);
-    } else if (type === 'Character') {
+    if (type === 'Character') {
       return getAllCharacter();
     } else if (type === 'commonFriends') {
       return getCharacter(id);
-    } else {
+    } else if (type === 'characterOfPeriod') {
+      return getPeriodCharactor(id);
+    }else {
       return null;
     }
   },
   (obj) => {
-    return obj.friends ? commonFriendsType : characterType;
+    if (obj.friends) {return commonFriendsType;}
+    else if (obj.period) {return characterOfPeriodType;}
+    else {return characterType;}
   }
 );
 
-/**
- * We define our basic ship type.
- *
- * This implements the following type system shorthand:
- *   type Ship : Node {
- *     id: String!
- *     name: String
- *   }
- */
-const shipType = new GraphQLObjectType({
-  name: 'Ship',
-  description: 'A ship in the Star Wars saga',
-  fields: () => ({
-    id: globalIdField('Ship'),
-    name: {
-      type: GraphQLString,
-      description: 'The name of the ship.',
-    },
-  }),
-  interfaces: [nodeInterface],
-});
 
-/**
- * We define a connection between a faction and its ships.
- *
- * connectionType implements the following type system shorthand:
- *   type ShipConnection {
- *     edges: [ShipEdge]
- *     pageInfo: PageInfo!
- *   }
- *
- * connectionType has an edges field - a list of edgeTypes that implement the
- * following type system shorthand:
- *   type ShipEdge {
- *     cursor: String!
- *     node: Ship
- *   }
- */
-const {
-  connectionType: shipConnection,
-  edgeType: ShipEdge,
-} = connectionDefinitions({name: 'Ship', nodeType: shipType});
-
-/**
- * We define our faction type, which implements the node interface.
- *
- * This implements the following type system shorthand:
- *   type Faction : Node {
- *     id: String!
- *     name: String
- *     ships: ShipConnection
- *   }
- */
-const factionType = new GraphQLObjectType({
-  name: 'Faction',
-  description: 'A faction in the Star Wars saga',
-  fields: () => ({
-    id: globalIdField('Faction'),
-    factionId: {
-      type: GraphQLString,
-      description: 'id of faction in db',
-      resolve: (faction) => {faction.id},
-    },
-    name: {
-      type: GraphQLString,
-      description: 'The name of the faction.',
-    },
-    ships: {
-      type: shipConnection,
-      description: 'The ships used by the faction.',
-      args: connectionArgs,
-      resolve: (faction, args) => connectionFromArray(
-        faction.ships.map((id) => getShip(id)),
-        args
-      ),
-    },
-  }),
-  interfaces: [nodeInterface],
-});
-
-/**
- * This is the type that will be the root of our query,
- * and the entry point into our schema.
- *
- * This implements the following type system shorthand:
- *   type Query {
- *     factions(names: [FactionName]): [Faction]
- *     node(id: ID!): Node
- *   }
- */
-// const queryType = new GraphQLObjectType({
-//   name: 'Query',
-//   fields: () => ({
-//     factions: {
-//       type: new GraphQLList(factionType),
-//       args: {
-//         names: {
-//           type: new GraphQLList(GraphQLString),
-//         },
-//       },
-//       resolve: (root, {names}) => getFactions(names),
-//     },
-//     node: nodeField,
-//   }),
-// });
 const commonFriendsType = new GraphQLObjectType({
   name: 'commonFriends',
   description: 'return two people common friends',
@@ -256,10 +63,20 @@ const commonFriendsType = new GraphQLObjectType({
   }),
   interfaces: [nodeInterface]
 });
-const {
-    connectionType: commonFriendsConnection,
-    edgeType: commonFriendsEdge
-} = connectionDefinitions({name: 'commonFriends', nodeType: commonFriendsType})
+
+const characterOfPeriodType = new GraphQLObjectType({
+  name: 'characterOfPeriod',
+  fields: () => ({
+    id: globalIdField('characterOfPeriod'),
+    period: {
+      type: GraphQLString
+    },
+    number: {
+      type: GraphQLInt
+    }
+  }),
+  interfaces: [nodeInterface]
+});
 const characterType = new GraphQLObjectType({
   name: 'Character',
   description: 'character model',
@@ -280,8 +97,13 @@ const characterType = new GraphQLObjectType({
         const commonFriends = getCharacter(peopleId[0]).friends.filter(function(n) {
           return getCharacter(peopleId[1]).friends.indexOf(n) != -1;
         });
-        console.log(commonFriends );
         return commonFriends.map((value) => getCharacter(value));
+      }
+    },
+    characterOfPeriod: {
+      type: new GraphQLList(characterOfPeriodType),
+      resolve: (data) => {
+        return getPeriodCharactors();
       }
     }
   }),
@@ -293,80 +115,10 @@ const queryType = new GraphQLObjectType({
   fields: () => ({
     character: {
       type: characterType,
-      resolve: () => {return getAllCharacter()}
+      resolve: () => {return getData()}
     },
     node: nodeField
   })
-});
-
-/**
- * This will return a GraphQLFieldConfig for our ship mutation.
- *
- * It creates these two types implicitly:
- *   input IntroduceShipInput {
- *     clientMutationId: string!
- *     shipName: string!
- *     factionId: ID!
- *   }
- *
- *   input IntroduceShipPayload {
- *     clientMutationId: string!
- *     ship: Ship
- *     faction: Faction
- *   }
- */
-const shipMutation = mutationWithClientMutationId({
-  name: 'IntroduceShip',
-  inputFields: {
-    shipName: {
-      type: new GraphQLNonNull(GraphQLString),
-    },
-    factionId: {
-      type: new GraphQLNonNull(GraphQLID),
-    },
-  },
-  outputFields: {
-    newShipEdge: {
-      type: ShipEdge,
-      resolve: (payload) => {
-        const ship = getShip(payload.shipId);
-        return {
-          cursor: cursorForObjectInConnection(
-            getShips(payload.factionId),
-            ship
-          ),
-          node: ship,
-        };
-      },
-    },
-    faction: {
-      type: factionType,
-      resolve: (payload) => getFaction(payload.factionId),
-    },
-  },
-  mutateAndGetPayload: ({shipName, factionId}) => {
-    const newShip = createShip(shipName, factionId);
-    return {
-      shipId: newShip.id,
-      factionId: factionId,
-    };
-  },
-});
-
-/**
- * This is the type that will be the root of our mutations,
- * and the entry point into performing writes in our schema.
- *
- * This implements the following type system shorthand:
- *   type Mutation {
- *     introduceShip(input: IntroduceShipInput!): IntroduceShipPayload
- *   }
- */
-const mutationType = new GraphQLObjectType({
-  name: 'Mutation',
-  fields: () => ({
-    introduceShip: shipMutation,
-  }),
 });
 
 /**
--- js/components/StarWarsShip.js
+++ js/components/StarWarsShip.js
@@ -1,17 +1,9 @@
-/**
- * This file provided by Facebook is for non-commercial testing and evaluation
- * purposes only.  Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
 import React from 'react';
 import Relay from 'react-relay';
+import ReactHighcharts from 'react-highcharts';
+//const ReactHighcharts = require('react-highcharts');
+console.log(ReactHighcharts );
 class StarWarsShip extends React.Component {
   handleChangeFirst = (e) => {
     this.props.relay.setVariables({firstPeople: e.target.value});
@@ -21,7 +13,34 @@ class StarWarsShip extends React.Component {
   };
   render() {
     const {character} = this.props;
-    console.log(character);
+    const config = {
+      chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+      title: {
+        text: 'Show every period the number of the characters'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+          }
+        }
+      },
+      series: [{
+        name: 'characters compare percent',
+        colorByPoint: true,
+        data: character.characterOfPeriod.map((value) => ({name: value.period, y: value.number}))
+      }]};
     let {firstPeople, secondPeople} = this.props.relay.variables;
     return <div>
       <select defaultValue={firstPeople} onChange={this.handleChangeFirst}>
@@ -37,10 +56,11 @@ class StarWarsShip extends React.Component {
         <option value="4">CHEWBACCA</option>
       </select>
       <h3>{character.commonfriends.map((value)=>value.name+' ')}</h3>
+      <ReactHighcharts config={config}></ReactHighcharts>
     </div>;
   }
 }
-
+// <ReactHighcharts config = {}></ReactHighcharts>
 export default Relay.createContainer(StarWarsShip, {
   initialVariables: {
     firstPeople: '1',
@@ -51,6 +71,10 @@ export default Relay.createContainer(StarWarsShip, {
       fragment on Character {
         commonfriends(firstPeople: $firstPeople, secondPeople: $secondPeople){
           name
+        },
+        characterOfPeriod {
+          period,
+          number
         }
       }
     `,
